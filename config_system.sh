864de5432631e4c8f425b55ed5ae0d907b890b43
Nikita Rudenko <rudenkornk@gmail.com>
2022-08-25 16:17:37 +0300

    [DEPLOY] Add more utils

--- config_system.sh
+++ config_system.sh
@@ -7,19 +7,36 @@ set -o xtrace
 
 apt-get update
 DEBAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends \
+  apt-transport-https \
   bash-completion \
+  bison \
+  ca-certificates \
+  ccache \
+  curl \
+  dos2unix \
   gdb \
   git \
+  gnupg \
+  jq \
   less \
+  lftp \
+  libevent-dev \
   make \
   ninja-build \
+  openssh-client \
+  pkg-config \
+  snap \
+  tar gzip zip unzip bzip2 p7zip-full p7zip-rar \
   valgrind \
   vim \
+  wget \
+  zlib1g-dev \
 
 rm -rf /var/lib/apt/lists/*
 
 pip install lit==14.0.3
 pip install gcovr
 pip install pathlib
+pip install psutil
 pip install pyyaml
 
