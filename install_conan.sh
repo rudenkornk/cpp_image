980decd34a92cab62fdb62f1bcedd686172d27e2
Nikita Rudenko <rudenkornk@gmail.com>
2022-08-30 16:02:50 +0300

    Update conan

--- install_conan.sh
+++ install_conan.sh
@@ -13,6 +13,6 @@ DEBAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends \
 wget https://github.com/mikefarah/yq/releases/download/v4.25.2/yq_linux_amd64 -O /usr/bin/yq
 chmod +x /usr/bin/yq
 
-pip install conan==1.49.0
+pip install conan==1.51.3
 
 rm -rf /var/lib/apt/lists/*
