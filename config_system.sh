90677e31d2f182e5d9703ce21e2d270f3a7b33ec
Nikita Rudenko <rudenkornk@gmail.com>
2022-10-18 17:41:29 +0300

    Add more packages

--- config_system.sh
+++ config_system.sh
@@ -16,7 +16,9 @@ DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends \
   curl \
   device-tree-compiler \
   dos2unix \
+  expat \
   expect \
+  flex \
   gdb \
   git \
   gnupg \
@@ -24,23 +26,31 @@ DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends \
   less \
   lftp \
   libevent-dev \
+  libfl-dev \
   libftdi-dev \
+  libgmp-dev \
   libhidapi-dev \
   libncurses5 \
   libtool \
-  libusb-dev \
   libusb-1.0-0-dev \
   linux-tools-generic \
+  lsb-release \
   make \
+  ncurses-dev \
   netcat \
   ninja-build \
+  opensbi \
   openssh-client \
+  openssh-server \
   pkg-config \
+  qemu-system-riscv64 \
+  qemu-utils \
   rsync \
   snap \
   sshpass \
   tar gzip zip unzip bzip2 p7zip-full p7zip-rar xz-utils \
   texinfo \
+  u-boot-qemu \
   valgrind \
   vim \
   wget \
